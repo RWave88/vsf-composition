bf1d2c0b396938ce7e40f59ecfd73c9d339eeab7
Andrew <andreyuniverse@gmail.com>
2021-09-01 22:48:28 +0300

    initial commit

---
+++ components/TestComposition.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+    Test
+  </div>
+</template>
+
+<script>
+import {useFetch} from '@nuxtjs/composition-api'
+
+export default {
+  name: 'TestComposition',
+  setup() {
+    useFetch(() => {})
+  }
+}
+</script>
